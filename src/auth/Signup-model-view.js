a46627277fb71597068961d26272d3029d4b66b0
toseefshahid <toseef.shahid@intagleo.com>
2020-04-27 12:12:59 +0500

    change base url

--- src/auth/Signup-model-view.js
+++ src/auth/Signup-model-view.js
@@ -49,7 +49,7 @@ class CATAudioModalView extends Component {
   }
 
   signUpButtonClicked = async (data) => {
-    fetch('http://127.0.0.1:8000/api/register', {
+    fetch('https://shielded-thicket-38521.herokuapp.com/api/register', {
       method: 'post',
       headers: {
         'Accept': 'application/json',
